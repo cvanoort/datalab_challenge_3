879f20f6f1fd28d086cf4a0f82dbfe9e447ebfd5
Colin Van Oort <cvanoort156@gmail.com>
2020-04-24 16:52:26 -0400

    Added calculation of doubling rates by region and made a few figures.

--- source/data.py
+++ source/data.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import numpy as np
 
 import pandas as pd
+import us
 
 
 def download_google_covid19_mobility(
@@ -15,16 +17,25 @@ def download_google_covid19_mobility(
 def load_google_covid19_mobility(
     path=Path("../data/google_covid19_mobility.csv"),
     update_window=pd.Timedelta(days=1),
+    clean=True,
+    mode="us",
 ):
+    assert mode in {"us", "states", "counties"}
+
     if not path.is_file() or stale_file(path, update_window=update_window):
         df = download_google_covid19_mobility()
     else:
         df = pd.read_csv(path, index_col=0)
-    df.date = pd.to_datetime(df.date)
+
+    if clean:
+        df = clean_google_covid19_mobility(df, mode=mode)
     return df
 
 
 def clean_google_covid19_mobility(df, mode="us"):
+    assert mode in {"us", "states", "counties"}
+
+    df.date = pd.to_datetime(df.date)
     df.rename(
         columns=lambda x: x.replace("_percent_change_from_baseline", ""), inplace=True
     )
@@ -47,30 +58,28 @@ def clean_google_covid19_mobility(df, mode="us"):
         us_df = us_df.set_index("Date").stack().reset_index()
         us_df.columns = ["Date", "Mobility Type", "Relative Change (%)"]
         return us_df
-    elif mode == "us_states":
-        us_states_ind = (
+    elif mode == "states":
+        states_ind = (
             (df.country_region_code == "US")
             & ~df.sub_region_1.isna()
             & df.sub_region_2.isna()
         )
-        us_states_df = (
-            df[us_states_ind]
+        states_df = (
+            df[states_ind]
             .dropna(axis=1)
             .drop(["country_region_code", "country_region"], axis=1)
         )
         # Clean up the column labels for a better legend later
-        us_states_df.rename(columns=lambda x: x.replace("_", " ").title(), inplace=True)
-        us_states_df = (
-            us_states_df.set_index(["Date", "Sub Region 1"]).stack().reset_index()
-        )
-        us_states_df.columns = [
+        states_df.rename(columns=lambda x: x.replace("_", " ").title(), inplace=True)
+        states_df = states_df.set_index(["Date", "Sub Region 1"]).stack().reset_index()
+        states_df.columns = [
             "Date",
             "Region",
             "Mobility Type",
             "Relative Change (%)",
         ]
-        return us_states_df
-    elif mode == "us_counties":
+        return states_df
+    elif mode == "counties":
         # counties_ind = (df.country_region_code == "US") & ~df.sub_region_1.isna() & ~df.sub_region_2.isna()
         raise NotImplementedError()
     else:
@@ -87,7 +96,7 @@ def download_covid_case_data(mode="us"):
 
 
 def load_covid_case_data(
-    mode="us", update_window=pd.Timedelta(days=1),
+    mode="us", update_window=pd.Timedelta(days=1), clean=True,
 ):
     assert mode in {"us", "states"}
     path = Path(f"../data/covid_cases_{mode}.csv")
@@ -96,8 +105,71 @@ def load_covid_case_data(
         df = download_covid_case_data(mode=mode)
     else:
         df = pd.read_csv(path, index_col=0)
-    df.date = pd.to_datetime(df.date)
+
+    if clean:
+        df = clean_covid_case_data(df, mode=mode)
     return df
+
+
+def clean_covid_case_data(df, mode="us"):
+    assert mode in {"us", "states"}
+    df.drop(["hash", "fips"], axis=1, inplace=True)
+    df.dropna(axis=1, inplace=True, how="all")
+    df.date = pd.to_datetime(df.date, format="%Y%m%d")
+    df.rename(columns={"date": "Date"}, inplace=True)
+    if mode == "states":
+        df.state = df.state.apply(
+            lambda x: us.states.lookup(x).name if us.states.lookup(x) else x
+        )
+        df.state = df.state.apply(lambda x: "District of Columbia" if x == "DC" else x)
+        df.rename(columns={"state": "Region"}, inplace=True)
+
+    return df
+
+
+def time_normalize(df, group_col="Region", indicator_col="positive"):
+    # Align data to date of first positive COVID case
+    chunks = dict()
+    for label, chunk in df.groupby(group_col):
+        chunk.set_index("Date", inplace=True)
+        chunk.sort_index(inplace=True)
+        chunk = chunk[chunk[indicator_col] > 0]
+        chunk.index = pd.Series(np.arange(len(chunk)), name="Days Since First Case")
+        if len(chunk):
+            chunks[label] = chunk
+        else:
+            print(f"No positive cases in {label}")
+    return chunks
+
+
+def get_covid_doubling_rates(group_col="Region", indicator_col="positive"):
+    state_case_df = load_covid_case_data(mode="states")
+    chunks = time_normalize(
+        state_case_df, group_col=group_col, indicator_col=indicator_col
+    )
+
+    doubling_rates = dict()
+    for label, chunk in chunks.items():
+        doubling_rates[label] = chunk[indicator_col] / (
+            1 + chunk[indicator_col + "Increase"].clip(0).rolling(7).mean()
+        )
+
+    chunk_len = (
+        (state_case_df[indicator_col] > 0)
+        .groupby(state_case_df[group_col])
+        .sum()
+        .astype(int)
+        .max()
+    )
+    drs = np.zeros((chunk_len, len(doubling_rates)), dtype=float)
+    drs[:] = np.nan
+    columns, values = zip(*doubling_rates.items())
+    for i, vs in enumerate(values):
+        drs[: len(vs), i] = vs
+
+    doubling_rates = pd.DataFrame(drs, columns=columns)
+    doubling_rates.index.name = "Days Since First Case"
+    return doubling_rates
 
 
 def stale_file(path, update_window=pd.Timedelta(days=1)):
--- source/eda.py
+++ source/eda.py
@@ -1,13 +1,44 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+import numpy as np
+from scipy import stats
 
 import data
 
 
 def main():
-    df = data.load_google_covid19_mobility()
+    doubling_rates = data.get_covid_doubling_rates().stack().reset_index()
+    doubling_rates.columns = ["Days Since First Case", "Region", "Days to Double"]
+    fig, ax = plt.subplots()
+    sns.lineplot(
+        x="Days Since First Case",
+        y="Days to Double",
+        hue="Region",
+        data=doubling_rates,
+        ax=ax,
+        legend=False,
+    )
+    plt.title("Doubling Rates by State/Territory")
+    plt.xticks(rotation=45, ha="right", va="top")
+    plt.yscale("log")
+    plt.tight_layout()
+    plt.savefig("../results/state_doubling.png")
+    plt.close(fig)
 
-    us_df = data.clean_google_covid19_mobility(df, mode="us")
+    double_bars = doubling_rates.groupby(["Region"]).mean().reset_index()
+    double_bars.sort_values("Days to Double", ascending=True, inplace=True)
+    fig, ax = plt.subplots()
+    sns.barplot(
+        x="Days to Double", y="Region", data=double_bars, ax=ax,
+    )
+
+    plt.title("Doubling Rates by State/Territory")
+    plt.tight_layout()
+    plt.savefig("../results/state_doubling_bars.png")
+    plt.close(fig)
+
+    us_df = data.load_google_covid19_mobility(mode="us")
     fig, ax = plt.subplots()
     sns.lineplot(
         x="Date", y="Relative Change (%)", hue="Mobility Type", data=us_df, ax=ax,
@@ -20,14 +51,10 @@ def main():
     plt.savefig("../results/us.png")
     plt.close(fig)
 
-    us_states_df = data.clean_google_covid19_mobility(df, mode="us_states")
+    states_df = data.load_google_covid19_mobility(mode="states")
     fig, ax = plt.subplots()
     sns.lineplot(
-        x="Date",
-        y="Relative Change (%)",
-        hue="Mobility Type",
-        data=us_states_df,
-        ax=ax,
+        x="Date", y="Relative Change (%)", hue="Mobility Type", data=states_df, ax=ax,
     )
     plt.title("Aggregated US State Mobility Patterns")
     plt.xticks(rotation=45, ha="right", va="top")
@@ -35,6 +62,96 @@ def main():
     plt.legend(fontsize="small")
     plt.tight_layout()
     plt.savefig("../results/states.png")
+    plt.close(fig)
+
+    us_case_df = data.load_covid_case_data(mode="us")
+    us_df = us_df.pivot(
+        index="Date", columns="Mobility Type", values="Relative Change (%)"
+    )
+    us_combined = pd.merge(us_df, us_case_df, on=["Date"], how="left")
+    us_combined.set_index("Date", inplace=True)
+
+    corr, labels = correlation_matrix(
+        us_combined.fillna(value=0),
+        group1=[
+            "Grocery And Pharmacy",
+            "Parks",
+            "Residential",
+            "Retail And Recreation",
+            "Transit Stations",
+            "Workplaces",
+        ],
+        group2=[
+            "pending",
+            "negative",
+            "positive",
+            "hospitalizedCumulative",
+            "inIcuCumulative",
+            "onVentilatorCumulative",
+            "death",
+            "recovered",
+        ],
+    )
+    plot_correlation_matrix(
+        corr,
+        tick_labels=labels,
+        title="US (Aggregated) Mobility-Covid Correlations",
+        tag="us_pearson",
+    )
+
+    state_case_df = data.load_covid_case_data(mode="states")
+    states_combined = pd.merge(
+        states_df, state_case_df, on=["Date", "Region"], how="left"
+    )
+
+
+def correlation_matrix(
+    df, group1=None, group2=None, ignore=None, correlation_func=stats.spearmanr
+):
+    columns = sorted(df.columns)
+    if ignore:
+        columns = sorted(set(columns) - set(ignore))
+
+    if not group1:
+        group1 = columns
+    if not group2:
+        group2 = columns
+
+    corr_mat = np.zeros((len(group1), len(group2)))
+    for i, col1 in enumerate(group1):
+        for j, col2 in enumerate(group2):
+            corr, *_ = correlation_func(df[col1].values, df[col2].values)
+            corr_mat[i, j] = corr
+    return corr_mat, [group1, group2]
+
+
+def plot_correlation_matrix(
+    corr_mat, tick_labels=None, axis_labels=None, title=None, cmap="viridis", tag=""
+):
+    fig, ax = plt.subplots()
+    plt.imshow(corr_mat, cmap=cmap, vmax=1, vmin=-1)
+    if tick_labels:
+        y_labels, x_labels = tick_labels
+        y_labels = [x.strip().replace("_", " ").title() for x in y_labels]
+        x_labels = [x.strip().replace("_", " ").title() for x in x_labels]
+        plt.yticks(np.arange(len(tick_labels[0])), y_labels)
+        plt.xticks(np.arange(len(tick_labels[1])), x_labels, rotation=45, ha="right")
+
+    if axis_labels:
+        plt.ylabel(axis_labels[0])
+        plt.xlabel(axis_labels[1])
+
+    if tag:
+        plt.title(tag.replace("_", " ").title())
+        tag = f"_{tag}"
+
+    if title:
+        plt.title(title)
+
+    plt.colorbar()
+    plt.tight_layout()
+
+    plt.savefig(f"../results/correlation{tag}.png")
     plt.close(fig)
 
 
