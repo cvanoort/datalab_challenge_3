Fixed some potential data leakage in the mobility regresion. This invalidated the previous regression results, but it appears that we can predict the number of new cases after adding a state indicator.

source/mobility_regression.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import LabelBinarizer, StandardScaler
 import data
 
 
-def main():
+def main(use_location=True):
     mobility = (
         data.load_google_covid19_mobility(mode="states")
         .set_index(["Date", "Region", "Mobility Type"])
@@ -23,7 +23,11 @@ def main():
     rates.columns = ["Date", "Region", "Doubling Rate"]
 
     x, y, state = prepare_regression_data(
-        mobility, rates, day_zeros, target="Doubling Rate"
+        mobility,
+        rates,
+        day_zeros,
+        target_col="Doubling Rate",
+        use_location=use_location,
     )
     model, score, split_ind = apply_regression(x, y)
     print(f"Test Score: {score}")
@@ -32,31 +36,63 @@ def main():
         model,
         columns=list(mobility.columns)[2:],
         lags=[f"T-{i}" for i in range(7, 0, -1)],
-        sub_line=f"Target = Case Doubling Rate\nR^2 = {score:0.4f}",
+        sub_line=f"Target = Case Doubling Rate @ T\nR^2 = {score:0.4f}",
+        tag="doubling_rate",
+    )
+
+    x, y, state = prepare_regression_data(
+        mobility,
+        covid,
+        day_zeros,
+        target_col="positiveIncrease",
+        use_location=use_location,
+    )
+    model, score, split_ind = apply_regression(x, y)
+    print(f"Test Score: {score}")
+
+    regression_coeff_plot(
+        model,
+        columns=list(mobility.columns)[2:],
+        lags=[f"T-{i}" for i in range(7, 0, -1)],
+        sub_line=f"Target = New Cases @ T\nR^2 = {score:0.4f}",
+        tag="new_cases",
     )
 
 
 def prepare_regression_data(
-    mobility, covid, day_zero, input_window=7, target="positive", use_location=False
+    feature_df,
+    target_df,
+    start_dates,
+    input_window=7,
+    target_col="positive",
+    use_location=False,
 ):
-    x, y, state = [], [], []
-    for region in sorted(set(mobility.Region) & set(covid.Region)):
-        start_date = day_zero[region]
-        state_mobility = mobility[
-            (mobility.Region == region) & (mobility.Date >= start_date)
+    xs, ys, states = [], [], []
+    for region in sorted(set(feature_df.Region) & set(target_df.Region)):
+        xs.append([]), ys.append([]), states.append([])
+        start_date = start_dates[region]
+        state_mobility = feature_df[
+            (feature_df.Region == region) & (feature_df.Date >= start_date)
         ].set_index("Date")
         state_mobility.drop(columns="Region", inplace=True)
-        state_covid = covid[covid.Region == region]
+        state_covid = target_df[target_df.Region == region]
 
         for i in range(len(state_mobility) - input_window):
-            state.append(region)
-            x.append(state_mobility.iloc[i : i + input_window].values.flatten())
-            y.append(state_covid[target].iloc[i + input_window])
+            states[-1].append(region)
+            xs[-1].append(state_mobility.iloc[i : i + input_window].values.flatten())
+            ys[-1].append(state_covid[target_col].iloc[i + input_window])
+
+    # Interleave xs, ys, and states to allow better handling of TS CV
+    x, y, state = [], [], []
+    for i in range(max(len(e) for e in ys)):
+        for x_, y_, state_ in zip(xs, ys, states):
+            if len(x_) > i:
+                x.append(x_[i]), y.append(y_[i]), state.append(state_[i])
 
     x = np.array(x)
     if use_location:
-        bin_state = LabelBinarizer().fit_transform(state)
-        x = np.concatenate([x, bin_state], axis=-1)
+        enc_state = LabelBinarizer().fit_transform(state)
+        x = np.concatenate([x, enc_state], axis=-1)
     return np.array(x), np.array(y), state
 
 
@@ -79,8 +115,12 @@ def apply_regression(x, y, test_size=0.2, test_size_band=0.1):
     return pipeline, pipeline.score(x_test, y_test), split_ind
 
 
-def regression_coeff_plot(model, columns, lags, sub_line=None):
-    coeffs = model[-1].coef_.reshape((len(lags), len(columns)))
+def regression_coeff_plot(
+    model, columns, lags, sub_line=None, tag=None,
+):
+    coeffs = (
+        model[-1].coef_[: len(columns) * len(lags)].reshape((len(lags), len(columns)))
+    )
     fig, ax = plt.subplots()
     # Reorient the array for a nicer display
     plt.imshow(coeffs.T, cmap="viridis")
@@ -94,7 +134,10 @@ def regression_coeff_plot(model, columns, lags, sub_line=None):
     plt.title(f"Regression Coefficients{sub_line}")
     plt.colorbar()
     plt.tight_layout()
-    plt.savefig("../results/regression_coeffs.png")
+
+    if tag:
+        tag = f"_{tag}"
+    plt.savefig(f"../results/regression_coeffs{tag}.png")
     plt.close(fig)
 
 