c41612ffe37566c62ac99806e38ab0a1945b2931
Colin Van Oort <cvanoort156@gmail.com>
2020-04-30 15:46:19 -0400

    Added a new regression over the daily COVID deaths. Added coefficient plots for all regressions to display the location related coefficients.

--- source/mobility_regression.py
+++ source/mobility_regression.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterable
+
+import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import ElasticNetCV
@@ -26,36 +29,61 @@ def main(use_location=True):
         mobility,
         rates,
         day_zeros,
+        input_window=21,
         target_col="Doubling Rate",
         use_location=use_location,
     )
     model, score, split_ind = apply_regression(x, y)
-    print(f"Test Score: {score}")
+    print(f"Doubling Rate Test Score: {score}")
 
     regression_coeff_plot(
         model,
         columns=list(mobility.columns)[2:],
-        lags=[f"T-{i}" for i in range(7, 0, -1)],
-        sub_line=f"Target = Case Doubling Rate @ T\nR^2 = {score:0.4f}",
+        lags=21,
+        sub_line=f"Target = Today's COVID Case Doubling Rate\nR^2 = {score:0.4f}",
         tag="doubling_rate",
+        figsize=(8, 4),
+        locations=sorted(set(state)),
     )
 
     x, y, state = prepare_regression_data(
         mobility,
         covid,
         day_zeros,
+        input_window=14,
         target_col="positiveIncrease",
         use_location=use_location,
     )
     model, score, split_ind = apply_regression(x, y)
-    print(f"Test Score: {score}")
+    print(f"New Cases Test Score: {score}")
 
     regression_coeff_plot(
         model,
         columns=list(mobility.columns)[2:],
-        lags=[f"T-{i}" for i in range(7, 0, -1)],
-        sub_line=f"Target = New Cases @ T\nR^2 = {score:0.4f}",
+        lags=14,
+        sub_line=f"Target = Today's New Covid Cases\nR^2 = {score:0.4f}",
         tag="new_cases",
+        locations=sorted(set(state)),
+    )
+
+    x, y, state = prepare_regression_data(
+        mobility,
+        covid,
+        day_zeros,
+        input_window=21,
+        target_col="deathIncrease",
+        use_location=use_location,
+    )
+    model, score, split_ind = apply_regression(x, y)
+    print(f"New Deaths Test Score: {score}")
+
+    regression_coeff_plot(
+        model,
+        columns=list(mobility.columns)[2:],
+        lags=21,
+        sub_line=f"Target = Today's COVID Deaths\nR^2 = {score:0.4f}",
+        tag="new_deaths",
+        locations=sorted(set(state)),
     )
 
 
@@ -93,10 +121,10 @@ def prepare_regression_data(
     if use_location:
         enc_state = LabelBinarizer().fit_transform(state)
         x = np.concatenate([x, enc_state], axis=-1)
-    return np.array(x), np.array(y), state
+    return x, np.array(y), state
 
 
-def apply_regression(x, y, test_size=0.2, test_size_band=0.1):
+def apply_regression(x, y, test_size=0.3, test_size_band=0.15):
     low = int((1 - test_size - test_size_band) * len(x))
     high = int((1 - test_size + test_size_band) * len(x))
     split_ind = np.random.randint(low, high)
@@ -107,38 +135,79 @@ def apply_regression(x, y, test_size=0.2, test_size_band=0.1):
         y[split_ind:],
     )
     pipeline = make_pipeline(
-        # PolynomialFeatures(degree=2, interaction_only=True),
-        StandardScaler(),
-        ElasticNetCV(max_iter=100_000, cv=TimeSeriesSplit()),
+        StandardScaler(), ElasticNetCV(max_iter=100_000, cv=TimeSeriesSplit()),
     )
     pipeline.fit(x_train, y_train)
     return pipeline, pipeline.score(x_test, y_test), split_ind
 
 
 def regression_coeff_plot(
-    model, columns, lags, sub_line=None, tag=None,
+    model, columns, lags, sub_line=None, tag=None, figsize=(8, 4), locations=None,
 ):
+    if not isinstance(lags, Iterable):
+        lags = list(range(-lags, 0, 1))
+
     coeffs = (
         model[-1].coef_[: len(columns) * len(lags)].reshape((len(lags), len(columns)))
     )
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=figsize)
     # Reorient the array for a nicer display
-    plt.imshow(coeffs.T, cmap="viridis")
+    plt.imshow(
+        coeffs.T, cmap="seismic", norm=MidpointNormalize(coeffs.min(), coeffs.max(), 0)
+    )
     plt.ylabel("Feature")
     plt.yticks(np.arange(len(columns)), columns)
-    plt.xlabel("Lag")
+    plt.xlabel("Time Lag (Days)")
     plt.xticks(np.arange(len(lags)), lags)
 
     if sub_line:
         sub_line = f"\n{sub_line}"
     plt.title(f"Regression Coefficients{sub_line}")
-    plt.colorbar()
+    plt.colorbar(orientation="horizontal")
     plt.tight_layout()
 
     if tag:
         tag = f"_{tag}"
     plt.savefig(f"../results/regression_coeffs{tag}.png")
     plt.close(fig)
+
+    if locations:
+        coeffs = model[-1].coef_[len(columns) * len(lags) :]
+        coeffs, locations = zip(
+            *sorted(zip(coeffs, locations), key=lambda x: abs(x[0]), reverse=True)
+        )
+        coeffs, locations = np.array(coeffs), np.array(locations)
+        inds = np.arange(len(coeffs))
+        fig, ax = plt.subplots(figsize=(12, 5))
+        plt.bar(
+            inds,
+            coeffs,
+            color=plt.cm.get_cmap("seismic")(
+                MidpointNormalize(coeffs.min(), coeffs.max(), 0)(coeffs)
+            ),
+        )
+        plt.ylabel("Coefficient")
+        plt.xlabel("State")
+        plt.xticks(inds, locations, rotation=45, ha="right")
+        plt.title(f"Regression Coefficients{sub_line}")
+        plt.tight_layout()
+        plt.savefig(f"../results/regression_coeffs{tag}_regions.png")
+        plt.close(fig)
+
+
+class MidpointNormalize(colors.Normalize):
+    """
+    Reference:
+        https://stackoverflow.com/questions/25500541/matplotlib-bwr-colormap-always-centered-on-zero
+    """
+
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
 
 
 if __name__ == "__main__":
